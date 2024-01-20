447837977ee45b87cfabcb2961926414a1be1ecf
muttyqt15 <95131963+muttyqt15@users.noreply.github.com>
2024-01-20 20:06:11 +0700

    responsive

--- aether/src/components/elements/Card.tsx
+++ aether/src/components/elements/Card.tsx
@@ -43,7 +43,7 @@ const Card = ({ id, title, description, image, author }: CardProps) => {
   return (
     <button
       onClick={handleClick}
-      className="text-[#3E3232] hover:scale-95 relative flex flex-col w-64 h-96 bg-[#C19A6B] border rounded-xl border-none text-2xl m-4 p-2 hover:shadow-md transition-all">
+      className="text-[#3E3232] hover:scale-95 relative flex flex-col w-96 h-96 md:w-64 md:h-96 bg-[#C19A6B] border rounded-xl border-none text-2xl m-4 p-2 hover:shadow-md transition-all">
       <section className="relative h-2/3 w-full border rounded-xl border-none">
         <Image
           src={image!}
@@ -53,7 +53,7 @@ const Card = ({ id, title, description, image, author }: CardProps) => {
           className="border rounded-xl border-none"
         />
       </section>
-      <h1 className="mt-2 font-bold sniglet text-[#5C4033]">{title}</h1>
+      <h1 className="mt-2 text-xl font-bold sniglet text-[#5C4033]">{title}</h1>
       <p className="text-sm">{description}</p>
       {userOwns && (
         <>
@@ -69,7 +69,7 @@ const Card = ({ id, title, description, image, author }: CardProps) => {
           </button>
         </>
       )}
-      <p className="text-xs absolute bottom-2 right-2">{author.username}</p>
+      <p className="text-xs absolute bottom-2 right-2 hidden md:flex">{author.username}</p>
     </button>
   );
 };
--- aether/src/pages/BooksSection.tsx
+++ aether/src/pages/BooksSection.tsx
@@ -8,7 +8,7 @@ const BooksSection = () => {
   const booksData = useBooksData();
   
   return (
-    <section className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 ">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
       {booksData.map((book) => (
         <Card
           key={book.id}
